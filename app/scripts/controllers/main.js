2a8b46f8f2d139f1956f3efb169f2890b4ca3992
pedroesperanca <buesimples@gmail.com>
2016-01-07 21:12:38 +0000

    infinite scroll behaviour

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,7 +15,9 @@ angular.module('pokedexApp')
     	$scope.pokemons = pokemons;
     });
     $scope.quantity = 12;
+    $scope.blockInfiniteScroll = true;
     $scope.getMore = function(){
     	$scope.quantity = $scope.quantity +12;
+    	$scope.blockInfiniteScroll = false;
     }
   });
